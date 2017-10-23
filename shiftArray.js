2bb4bb9ff2a5c3e9f9d1b5fe5ecdc8d2156f9e63
Sereina Blanchard <wintermist3@gmail.com>
2017-10-23 09:39:14 -0700

    updated Shfit Array

--- shiftArray.js
+++ shiftArray.js
@@ -1,20 +1,35 @@
-// In progress
-
+// Using extra array
 function shiftArray(arr, x) {
+    var newArr = [];
 
-    var temp = 0;
-    var counter = x;
-
-    while(counter > 0) {
-        for(var i = 0; i < arr.length; i++) {
-            temp = arr[i];
-            arr[i] = arr[i+1];
-            arr[i+1] = temp;
-
-        }
-        counter--;
+    for(var i = arr.length-x; i < arr.length; i++) {
+        newArr.push(arr[i]);
     }
-    return arr;
+    for(var i = 0; i < arr.length-x; i++) {
+        newArr.push(arr[i]);
+    }
+
+    return newArr;
 }
 
 console.log(shiftArray([1,2,3,4,5,6], 2));
+
+
+// In place +++ In progress
+
+// function shiftArray(arr, x) {
+
+//     var temp = 0;
+//     var counter = x;
+
+//     while(counter > 0) {
+//         for(var i = 0; i < arr.length; i++) {
+//             temp = arr[i];
+//             arr[i] = arr[i+1];
+//             arr[i+1] = temp;
+
+//         }
+//         counter--;
+//     }
+//     return arr;
+// }
